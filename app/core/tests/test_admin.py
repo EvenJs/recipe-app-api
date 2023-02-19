fe18e8000bc13de7e5fbd0e4a52cc0f3092faf4f
Johnson <yuhang0229@gmail.com>
2023-02-19 12:47:44 +1100

    fix lint

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -4,20 +4,21 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
     def setUp(self):
         """Create user and clients."""
         self.client = Client()
-        self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'testpass123'
+        self.admin_user=get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123'
         )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name = 'Test User'
+        self.user=get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
         )
 
     def test_users_list(self):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,8 +14,8 @@ class ModelTests(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-        email=email,
-        password=password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
@@ -30,13 +30,13 @@ class ModelTests(TestCase):
             ['test4@Example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raise_error(self):
         """Test that creating a user without and email raise a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
         """Test creating a superuser"""
